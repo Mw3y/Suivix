29eb4beea90de69ce8db5cea8b93a81f9a7236a5
Max <maxence1604.minecraft@gmail.com>
2021-03-01 12:24:09 +0100

    Changelog

--- routes/models/api/changelog.js
+++ routes/models/api/changelog.js
@@ -8,7 +8,7 @@ var package = require('../../../package.json');
 module.exports = (req, res) => {
     res.send({
         version: package.version,
-        en: "• The poll creation feature is now accessible for everyone!<br>Accessible at <a href='https://suivix.xyz/poll'>https://suivix.xyz/poll</a>.<br><br>• Multiple bug fixes on the poll feature and website upgrades.<br><br><span class='moreInfos'>If you encounter a bug, or you think something is broken, please contact me on Discord: MΛX#2231</span>",
-        fr: "• La création de sondages est maintenant disponible pour tout le monde !<br>Rendez-vous sur <a href='https://suivix.xyz/poll'>https://suivix.xyz/poll</a>.<br><br>• Corrections de bugs sur les sondages et améliorations globales du site web.<br><br><span class='moreInfos'>Si vous rencontrez un bug, ou pensez que quelque chose ne marche pas bien, merci de me contacter sur Discord : MΛX#2231</span>"
+        en: "• New attendance result design.<br>• The csv file is now sent to Discord.<br><br><span class='moreInfos'>If you encounter a bug, or you think something is broken, please contact me on Discord: MΛX#2231</span>",
+        fr: "• Refonte totale de l'affichage du résultat du suivi.<br>• Le fichier csv est désormais envoyé sur Discord.<br><br><span class='moreInfos'>Si vous rencontrez un bug, ou pensez que quelque chose ne marche pas bien, merci de me contacter sur Discord : MΛX#2231</span>"
     });
 };
